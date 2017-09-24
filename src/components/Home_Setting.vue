f55e24bba6e096fcf3bdfab7dfeecabd4523137a
刘白菜 <liushuai.baicai@hotmail.com>
2017-09-24 14:42:54 +0800

    update 设置项ui

--- src/components/Home_Setting.vue
+++ src/components/Home_Setting.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
+      <div>
+        <mt-cell class="setting-item" title="设置1">
+          <mt-switch v-model="value1"></mt-switch>
+        </mt-cell>
+      </div>
+      <div>
+        <mt-cell class="setting-item" title="标题" is-link></mt-cell>
+      </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+      data () {
+        return {
+          value1: true
+        }
+      }
+    }
 </script>
 
 <style scoped>
+  .setting-item {
+    padding: 10px 0;
+  }
 </style>
